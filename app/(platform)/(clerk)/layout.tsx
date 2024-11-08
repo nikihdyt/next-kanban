cc7ccb1a5aa4cc027c086b8ae65b27aae13e3ffb
nikihdyt <nikihidayati20@gmail.com>
2024-11-08 20:18:41 +0800

    style(auth): center form

---
+++ app/(platform)/(clerk)/layout.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ClerkLayout = ({
+    children
+}: {
+    children: React.ReactNode
+}) => {
+  return (
+    <div className='w-full h-full
+    flex items-center justify-center'>
+        {children}
+    </div>
+  )
+}
+
+export default ClerkLayout
